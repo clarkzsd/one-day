cf6a3bb42de4e828a5913d8a13415d28b3ad3e36
clarkzsd <clarkzsd@gmail.com>
2018-03-14 16:28:02 +0800

    Optimize FloatingButton

--- src/components/UI/FloatingButton/index.js
+++ src/components/UI/FloatingButton/index.js
@@ -4,16 +4,18 @@ import './style.scss';
 
 class FloatingButton extends Component {
   render () {
+    const { icon, onPress } = this.props;
     return (
-      <div className='floating-action-button'>
-        <i className='material-icons'>{this.props.icon}</i>
+      <div className='floating-action-button' onClick={onPress}>
+        <i className='material-icons'>{icon}</i>
       </div>
     );
   }
 }
 
 FloatingButton.propTypes = {
-  icon: PropTypes.string.isRequired
+  icon: PropTypes.string.isRequired,
+  onPress: PropTypes.func
 };
 
 export default FloatingButton;
