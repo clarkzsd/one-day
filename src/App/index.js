dbfbccbe5af6beb279b882ac80348372d6d29d53
clarkzsd <clarkzsd@gmail.com>
2018-03-14 22:46:24 +0800

    Add trasition for router switching

--- src/App/index.js
+++ src/App/index.js
@@ -1,19 +1,31 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { hot } from 'react-hot-loader';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import TodoListScreen from '../screen/TodoListScreen';
 import CreateTodoScreen from '../screen/CreateTodoScreen';
+import './style.scss';
 
 class App extends Component {
   render () {
     return (
       <BrowserRouter>
-        <div>
-          <Switch>
-            <Route exact path='/' component={TodoListScreen} />
-            <Route exact path='/create' component={CreateTodoScreen} />
-          </Switch>
-        </div>
+        <Route
+          render={({ location }) => (
+            <TransitionGroup>
+              <CSSTransition
+                key={location.key}
+                classNames='fade'
+                timeout={300}
+              >
+                <Switch location={location}>
+                  <Route exact path='/' component={TodoListScreen} />
+                  <Route exact path='/create' component={CreateTodoScreen} />
+                </Switch>
+              </CSSTransition>
+            </TransitionGroup>
+          )}
+        />
       </BrowserRouter>
     );
   }
