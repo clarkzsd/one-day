1e178acd9db4e816ec64f2492bf224124dce152a
clarkzsd <clarkzsd@gmail.com>
2018-03-15 21:58:25 +0800

    Add TodoListEmpty

---
+++ src/components/TodoList/TodoListEmpty/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.scss';
+
+const TodoListEmpty = () => (
+  <div className='empty-indictor'>
+    <div className='inner'>
+      找点事儿做吧~
+    </div>
+  </div>
+);
+
+export default TodoListEmpty;
