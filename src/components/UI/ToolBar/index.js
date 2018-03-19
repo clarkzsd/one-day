e65558425d6875cdef4189f96b843d0097e6424d
clarkzsd <clarkzsd@gmail.com>
2018-03-19 13:40:31 +0800

    Fix ToolBar

--- src/components/UI/ToolBar/index.js
+++ src/components/UI/ToolBar/index.js
@@ -2,13 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './style.scss';
 
-const ToolBar = ({ left, right, title, onLeftPress }) => (
-  <header className='tool-bar'>
+const ToolBar = ({ left, right, title, onLeftPress, style, titleStyle }) => (
+  <header className='tool-bar' style={style}>
     <div className='inner'>
       <button className='left-btn' onClick={onLeftPress}>
         {left}
       </button>
-      <div className='screen-title'>{title}</div>
+      <div className='screen-title'>
+        <span className='screen-title-content' style={titleStyle}>
+          {title}
+        </span>
+      </div>
       {
         right
           ? <button className='left-btn' onClick={onLeftPress}>{left}</button>
@@ -22,7 +26,9 @@ ToolBar.propTypes = {
   left: PropTypes.object.isRequired,
   right: PropTypes.object,
   title: PropTypes.string,
-  onLeftPress: PropTypes.func
+  onLeftPress: PropTypes.func,
+  style: PropTypes.object,
+  titleStyle: PropTypes.object
 };
 
 export default ToolBar;
