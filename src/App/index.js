e7bcd4d2f53505122899a7322d296a67c88c90f5
clarkzsd <clarkzsd@gmail.com>
2018-03-28 10:29:16 +0800

    Disable viewport scaling in iOS10+ Safari

--- src/App/index.js
+++ src/App/index.js
@@ -6,9 +6,18 @@ import AppRoute from '../AppRoute';
 
 class App extends Component {
   componentDidMount () {
+    // Remove loading spinner
     const body = document.querySelector('body');
     const spinner = document.getElementById('loading-spinner');
     body.removeChild(spinner);
+
+    // iOS10+ Safari viewport zooming hack
+    // Docs: https://webkit.org/blog/7367/new-interaction-behaviors-in-ios-10/
+    document.addEventListener('touchmove', (event) => {
+      if (event.scale !== 1) {
+        event.preventDefault();
+      }
+    }, false);
   }
   render () {
     return (
