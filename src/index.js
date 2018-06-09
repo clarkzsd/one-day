73a79bebe03cc7fc964018b3aa5afd50328ab214
clark <clarkzsd@gmail.com>
2018-06-09 16:55:55 +0800

    Use color variables

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './normalize.css';
-import './global.scss';
+import './styles/normalize.css';
+import './styles/global.scss';
 
 const render = Component => {
   ReactDOM.render(
