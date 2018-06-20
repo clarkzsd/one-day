0083b9703078b16f71d197072cd0d2e7c762a467
clark <clarkzsd@gmail.com>
2018-06-20 09:55:33 +0800

    Add Header component

---
+++ src/components/UI/Header/index.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './style.scss';
+
+const Header = ({
+  title,
+  leftIcon,
+  onLeftPress,
+  onRightPress,
+  titleStyle,
+  rightIcon,
+  largeTitle
+}) => (
+  <header className='header'>
+    <div className='header__toolBar'>
+      <div className='inner'>
+        {
+          leftIcon &&
+          <button className='header__leftBtn' onClick={onLeftPress}>
+            {leftIcon}
+          </button>
+        }
+        <div className='header__title'>
+          <span style={titleStyle}>
+            {title}
+          </span>
+        </div>
+        {
+          rightIcon
+            ? <button className='header__rightBtn' onClick={onRightPress}>{rightIcon}</button>
+            : <div className='header__rightBtn' style={{ width: '24px', height: '24px' }} />
+        }
+      </div>
+    </div>
+    <div className='header__largeTitle'>
+      <div className='inner'>
+        {largeTitle}
+      </div>
+    </div>
+  </header>
+);
+
+Header.propTypes = {
+  leftIcon: PropTypes.object,
+  rightIcon: PropTypes.object,
+  title: PropTypes.string,
+  onLeftPress: PropTypes.func,
+  onRightPress: PropTypes.func,
+  titleStyle: PropTypes.object,
+  largeTitle: PropTypes.string
+};
+
+export default Header;
