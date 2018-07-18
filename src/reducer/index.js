09591a9cf38440c2fc17cc295a2d6ee91bcc5876
clark <clarkzsd@gmail.com>
2018-07-18 16:57:19 +0800

    Add StatisticsScreen

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -2,14 +2,15 @@ import { combineReducers } from 'redux';
 import homeReducer from '../screens/TodoListScreen/reducer';
 import projectReducer from '../screens/ProjectScreen/reducer';
 import appReducer from '../screens/App/reducer';
-
 import UIReducer from '../components/reducer';
+import statisticsReducer from '../screens/StatisticsScreen/reducer';
 
 const rootReducer = combineReducers({
   app: appReducer,
   home: homeReducer,
   ui: UIReducer,
-  project: projectReducer
+  project: projectReducer,
+  statistics: statisticsReducer
 });
 
 export default rootReducer;
---
+++ src/screens/StatisticsScreen/index.js
@@ -0,0 +1,186 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import echarts from 'echarts/lib/echarts';
+import 'echarts/lib/chart/bar';
+import 'echarts/lib/chart/pie';
+import 'echarts/lib/component/tooltip';
+import 'echarts/lib/component/title';
+
+// component
+import Tabs from '../../components/UI/Tabs';
+import Header from '../../components/UI/Header';
+
+// action
+import { openDrawer } from '../../components/action';
+import { fetchStatistics } from './action';
+
+import Const from '../../base/constants';
+import './style.scss';
+
+const TabsPanel = Tabs.TabsPanel;
+
+let todayBarChart, todayPieChart, weekBarChart, weekPieChart;
+
+class StatisticsScreen extends Component {
+  static propTypes = {
+    openDrawer: PropTypes.func,
+    fetchStatistics: PropTypes.func,
+    todayStatistics: PropTypes.object,
+    weekStatistics: PropTypes.object
+  }
+
+  componentDidMount () {
+    todayBarChart = echarts.init(this.todayBarChart);
+    todayPieChart = echarts.init(this.todayPieChart);
+    weekBarChart = echarts.init(this.weekBarChart);
+    weekPieChart = echarts.init(this.weekPieChart);
+    const { fetchStatistics } = this.props;
+    Promise.all([
+      fetchStatistics('today'),
+      fetchStatistics('week')
+    ]).then(() => {
+      const { todayStatistics, weekStatistics } = this.props;
+      const todayData = todayStatistics.data;
+      const weekData = weekStatistics.data;
+      this.setBarChartOption(todayBarChart, Const.date.twentyFourHours, todayData.data.todayStatisticsList);
+      this.setPieChartOption(todayPieChart, [
+        {value: todayData.data.specificData.unfinished, name: '未完成'},
+        {value: todayData.data.specificData.finished, name: '已完成'}
+      ]);
+      this.setBarChartOption(weekBarChart, Const.date.sevenWeekDays, weekData.data.last7DaysStatisticsList);
+      this.setPieChartOption(weekPieChart, [
+        {value: weekData.data.specificData.unfinished, name: '未完成'},
+        {value: weekData.data.specificData.finished, name: '已完成'}
+      ]);
+    });
+  }
+
+  setPieChartOption (chart, data) {
+    if (chart.setOption) {
+      chart.setOption({
+        color: ['#009688'],
+        roseType: 'radius',
+        series: [
+          {
+            type: 'pie',
+            radius: '45%',
+            center: ['50%', '50%'],
+            roseType: 'radius',
+            data: data.sort(function (a, b) { return a.value - b.value; })
+          }
+        ]
+      });
+    }
+  }
+
+  setBarChartOption (chart, xAxisData, data) {
+    if (chart.setOption) {
+      chart.setOption({
+        color: ['#03A9F4'],
+        dataZoom: [
+          {
+            type: 'inside',
+            xAxisIndex: [0],
+            start: 1,
+            end: 35
+          }
+        ],
+        xAxis: {
+          data: xAxisData
+        },
+        yAxis: {},
+        series: [
+          {
+            name: '任务个数',
+            type: 'bar',
+            data: data
+          }
+        ]
+      });
+    }
+  }
+
+  handleTabClick = (key) => {
+    if (key === '1') {
+      console.log('trigger');
+      weekBarChart.resize({
+        width: 343
+      });
+      weekPieChart.resize({
+        width: 206,
+        height: 250
+      });
+    }
+  }
+
+  renderDataChart (time) {
+    const statistics = this.props[`${time}Statistics`];
+    const statisticsData = statistics.data.data;
+    return (
+      <div className='statisticsContainer__content'>
+        <div className='statisticsContainer__barChart' ref={(ele) => { this[`${time}BarChart`] = ele; }} />
+        <div className='statisticsContainer__percentageData'>
+          <div className='statisticsContainer__pieChart' ref={(ele) => { this[`${time}PieChart`] = ele; }} />
+          <div className='statisticsContainer__taskData'>
+            {Const.task.taskStatisticsStatusList.map((item) => (
+              <div className='taskDataItem' key={item.key}>
+                <span className={`taskDataItem__number taskDataItem__number--${item.value}`}>
+                  {
+                    statisticsData ? statisticsData.specificData[item.value] : 'loading'
+                  }
+                </span>
+                <span className='taskDataItem__label'>{item.value.toUpperCase()}</span>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  render () {
+    const { openDrawer } = this.props;
+    return (
+      <div className='statisticsContainer'>
+        <Header
+          largeTitle='数据统计'
+          leftIcon={<i className='material-icons'>menu</i>}
+          onLeftPress={openDrawer}
+        />
+        <Tabs
+          onTabClick={this.handleTabClick}
+          defaultActiveKey='0'>
+          {
+            Const.date.statisticDateList.map((item, index) => {
+              return (
+                <TabsPanel
+                  key={index}
+                  panelKey={`${index}`}
+                  tabTitle={item.value.toUpperCase()}>
+                  {this.renderDataChart(item.value)}
+                </TabsPanel>
+              );
+            })
+          }
+        </Tabs>
+      </div>
+    );
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    openDrawer: () => dispatch(openDrawer()),
+    fetchStatistics: (type) => dispatch(fetchStatistics(type))
+  };
+};
+
+const mapStateToProps = ({ statistics }) => {
+  return {
+    todayStatistics: statistics.today,
+    weekStatistics: statistics.week
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(StatisticsScreen);
